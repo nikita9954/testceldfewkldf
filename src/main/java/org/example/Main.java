886d7f9ebe193b32923149c951d6771caae20cb0
m.kuniytsa <rejfrejnkf@gmail.com>
2024-11-25 18:35:39 +0300

    Коммит по первому заданию

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,17 +6,10 @@ import java.util.Locale;
 public class Main {
     public static void main(String[] args) {
 
-        int counter = 1;
-        int[][] table = new int[3][4];
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 4; j++) {
-                table[i][j] = counter;
-                System.out.print(table[i][j] + " ");
-                counter++;
-            }
-            System.out.println();
 
-        }
+        System.out.println("Задание 1");
+
     }
+
 }
 
